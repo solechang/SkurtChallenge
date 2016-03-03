904f83e3698abe2ecc2b604fd37b0d14b79746a5
Chang <changwoo92@gmail.com>
2016-03-02 23:01:26 -0800

    added skurt

---
+++ SkurtChallenge/SkurtChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SkurtChallenge
+//
+//  Created by Chang Choi on 3/2/16.
+//  Copyright © 2016 solechang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
