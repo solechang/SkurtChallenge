3a0b0a04a1602a104a7ca9b33a9a09587e369b58
Chang <changwoo92@gmail.com>
2016-03-03 20:01:08 -0800

    Work

---
+++ SkurtChallenge/SkurtChallenge/FlightTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FlightTableViewController.h
+//  SkurtChallenge
+//
+//  Created by Chang Choi on 3/2/16.
+//  Copyright © 2016 solechang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlightTableViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@end
